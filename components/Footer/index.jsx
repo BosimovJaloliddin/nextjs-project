0d98ebbeb960b2a6aef62aa860f4ca2ece9ffe19
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-08-14 11:34:35 +0500

    add footer component

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,15 +1,57 @@
 import React from "react";
+import {
+  Contacts,
+  Container,
+  FooterInfo,
+  Icon,
+  Logo,
+  MenuItem,
+  MenuItems,
+  Register,
+  Wrapper,
+} from "./style";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div
-      className="container"
-      style={{
-        marginTop: "100px",
-      }}
-    >
-      <h1>Footer</h1>
-    </div>
+    <Container className="container">
+      <Wrapper>
+        <Logo>
+          <span className="logo-trip">Trip</span>
+          <span className="logo-goal">Goal</span>
+        </Logo>
+        <MenuItems>
+          <MenuItem>
+            <Link href="/">Home</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/about">About</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/packages">Packages</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/contact">Contact Us</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/faq">FAQ</Link>
+          </MenuItem>
+        </MenuItems>
+        <Contacts>
+          <Link target="_blanck" href="https://www.facebook.com/">
+            <Icon.Facebook />
+          </Link>
+          <Link target="_blanck" href="https://www.instagram.com/">
+            <Icon.Instagram />
+          </Link>
+          <Link target="_blanck" href="https://www.telegram.com/">
+            <Icon.Telegram />
+          </Link>
+        </Contacts>
+      </Wrapper>
+      <hr />
+      <FooterInfo> © 2021. All copyrights reserved by RH Agency</FooterInfo>
+    </Container>
   );
 };
 
---
+++ components/Footer/style.js
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+
+import facebook from "@/assets/icons/facebook.svg";
+import instagram from "@/assets/icons/instagram.svg";
+import telegram from "@/assets/icons/telegram.svg";
+
+export const Container = styled.footer`
+  padding-top: 100px;
+  padding-bottom: 50px;
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 15px;
+  padding-bottom: 35px;
+`;
+export const Logo = styled.div`
+  .logo-trip {
+    font-family: "Inter";
+    font-size: 20px;
+    font-weight: 800;
+    color: var(--primary-color);
+  }
+  .logo-goal {
+    font-family: "Inter";
+    font-size: 20px;
+    font-weight: 800;
+    color: var(--primary-text-color);
+
+    &::after {
+      display: inline-block;
+      margin-left: 5px;
+      content: "";
+      width: 8px;
+      height: 8px;
+      background-color: #00b3a0;
+      border-radius: 50%;
+    }
+  }
+  user-select: none;
+  -webkit-user-select: none;
+  cursor: context-menu;
+`;
+export const MenuItems = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  gap: 24px;
+  list-style: none;
+`;
+export const MenuItem = styled.li`
+  a {
+    position: relative;
+    font-family: "inter", sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 18px;
+    text-align: left;
+    text-decoration: none;
+    color: var(--primary-text-color);
+    cursor: pointer;
+    transition: all 0.1s ease;
+    user-select: none;
+    -webkit-user-select: none;
+
+    &:hover {
+      color: var(--primary-color);
+    }
+  }
+`;
+export const Contacts = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 18px;
+`;
+export const Icon = styled.div``;
+Icon.Facebook = styled(facebook)`
+  &:hover {
+    & path {
+      fill: var(--primary-color);
+    }
+  }
+`;
+Icon.Instagram = styled(instagram)`
+  &:hover {
+    & path {
+      fill: var(--primary-color);
+    }
+  }
+`;
+Icon.Telegram = styled(telegram)`
+  &:hover {
+    & path {
+      fill: var(--primary-color);
+    }
+  }
+`;
+
+export const FooterInfo = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 14px;
+  text-align: center;
+  margin-top: 30px;
+  color: #697687;
+`;
